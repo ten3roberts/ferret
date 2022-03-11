b0a143b6c8a28b4f819d587f8416afd83986deda
Tei Roberts <ten3roberts@gmail.com>
2022-03-11 22:28:36 +0100

    Tailwind header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,14 @@
+<script>
+  const routes = ["About", "Home"];
+</script>
+
+<nav class="shadow">
+  <ol class="flex justify-start items-center">
+    <h1 class="font-bold px-5">Ferret</h1>
+    {#each routes as route}
+      <li class="py-2 px-3 hover:bg-emerald-400">
+        <a href={"/" + route.toLowerCase()}>{route}</a>
+      </li>
+    {/each}
+  </ol>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
-<h1 class="font-bold text-fuchsia-400">Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  import Header from "../lib/Header.svelte";
+</script>
+
+<Header />
